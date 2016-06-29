d12c68ee611cc93f799c8983816015dad64b4ba7
tonuidavies <tonuidavies@gmail.com>
2016-06-29 23:53:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    #querying db
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
